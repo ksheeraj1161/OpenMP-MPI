d6006909d137a640572b4f2671d4dfacc63ecff6
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-12-16 18:12:42 +0530

    Create program1.c

---
+++ OpenMP/program1.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]){
+ omp_set_num_threads(4);
+ printf(“Print about PDC lab# 1\n”);
+#pragma omp parallel
+{
+printf(“Print about PDC lab #2\n”);
+#pragma omp for
+for (i=0;i<5;i++) {
+ 	printf(“Print about PDC lab #3\n”);
+ 	}
+printf(“Print about PDC lab #4\n”);
+} 
+return 0;
+}
